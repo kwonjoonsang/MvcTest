bf6e9506aa5ceab8a8d162f58216c093e0242cde
YSFather <basketlinux@nate.com>
2024-11-30 22:28:40 +0900

    Model 추가

---
+++ Data/MvcTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcTest.Models;
+
+namespace MvcTest.Data
+{
+    public class MvcTestContext : DbContext
+    {
+        public MvcTestContext (DbContextOptions<MvcTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcTest.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcTest.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MvcTestContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcTestContext") ?? throw new InvalidOperationException("Connection string 'MvcTestContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
